9b0d8ea316faf051946a32e8cf3e6c0e2f94b5e7
kermitchen <kermitchen@liscotech.com>
2019-10-25 16:53:59 +0800

    Add: class function

--- verilog_IO_linker.py
+++ verilog_IO_linker.py
@@ -1,50 +1,69 @@
 import numpy as np
 
 
+class class__verilog_IO_linker():
+	def __init__(self, fp):
+		self.fp = fp
+		self.rangeCM = 0
+		self.remain_txt = ""
+		self.words = []
+		self.num_module = 0
+		self.moduleName = ""
+		self.nextWord_is_moduleName = 0
+	
+	def scan_all(self):
+		for lineTxt in iter(self.fp):
+			self.remove_commet(lineTxt)
+			if (self.rangeCM==0):
+				self.get_words()
+				self.get_modueName()
 
-def main():
-	fp = open("D:\\DevProjects\\anaconda\\verilog_IO_linker\\axis_async_fifo_adapter.v", "r")
-
-	num_module = 0
-	rangeCM = 0
-	PF__moduleName = 0
-	moduleName = ""
-
-	for lineTxt in iter(fp):
+	
+	
+	def remove_commet(self,lineTxt):
 		remain_txt = ""
-		if (rangeCM==0):
+		if (self.rangeCM==0):
 			if ("/*") in lineTxt:
 				if ("*/") in lineTxt:
-					rangeCM = 0
+					self.rangeCM = 0
 					remain_txt = remain_txt + lineTxt.split("/*")[0] + " "
 					remain_txt += lineTxt.split("*/")[1]
 					a=1
 				else:
-					rangeCM = 1
+					self.rangeCM = 1
 			else:
 				remain_txt = lineTxt
 		else:
 			if ("*/") in lineTxt:
-				rangeCM = 0
+				self.rangeCM = 0
 				remain_txt = lineTxt.split("*/")[1]
 		remain_txt = remain_txt.split("//")[0]
+		self.remain_txt = remain_txt
+		
+	def get_words(self):
+		self.words = self.remain_txt.split(" ")
+
+	def get_modueName(self):
+		for word in self.words:
+			if (word=="module"):
+				self.num_module+=1
+				self.nextWord_is_moduleName = 1
+			else:
+				if (self.nextWord_is_moduleName):
+					self.nextWord_is_moduleName = 0
+					self.moduleName = word
+
+	
 
 
-		if (rangeCM==0):
-			parse_keywords = remain_txt.split(" ")
-			for parse_keyword in parse_keywords:
-				if (PF__moduleName==1):
-					PF__moduleName = 0
-					moduleName = parse_keyword
 
+def main():
+	fp = open("D:\\DevProjects\\anaconda\\verilog_IO_linker\\axis_async_fifo_adapter.v", "r")
+	VIOL = class__verilog_IO_linker(fp)
 
-				if (parse_keyword=="module"):
-					num_module+=1
-					PF__moduleName = 1
-				
+	VIOL.scan_all()
 
 	fp.close()
-	a=0
 
 if __name__ =="__main__":
 	main()
